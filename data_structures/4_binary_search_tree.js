0b16a357d1e797fb7ea0ee7473cc29a947e16d6b
aavetisyanit <aavetisyanit@gmail.com>
2021-03-16 23:01:37 -0500

    BST insert method

--- data_structures/4_binary_search_tree.js
+++ data_structures/4_binary_search_tree.js
@@ -4,6 +4,17 @@ class BinarySearchTreeNode {
 		this.left = null;
 		this.right = null;
 	}
+	insert(data) {
+		if (this.data >= data) {
+			return this.left
+				? this.left.insert(data)
+				: (this.left = new BinarySearchTreeNode(data));
+		} else if (this.data < data) {
+			return this.right
+				? this.right.insert(data)
+				: (this.right = new BinarySearchTreeNode(data));
+		}
+	}
 	contains(data) {
 		if (this.data === data) return this;
 		if (this.data > data) {
