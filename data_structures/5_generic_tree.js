348ee0db130b1926b2387d0d51ee7712fd228d51
aavetisyanit <aavetisyanit@gmail.com>
2021-04-10 17:42:59 -0500

    Implement stack

--- data_structures/5_generic_tree.js
+++ data_structures/5_generic_tree.js
@@ -1,7 +1,30 @@
+class GenericTreeNode {
+	constructor(data) {
+		this.data = data;
+		this.children = [];
+	}
+	add(data) {
+		this.children.push(new GenericTreeNode(data));
+	}
+	remove(data) {
+		this.children = this.children.filter(node => node.data !== data);
+	}
+}
+
 class GenericTree {
 	constructor() {
 		this.root = null;
 	}
+	breadthFirstTraversal(fn) {
+		const arrayHolder = [this.root];
+
+		while (arrayHolder.length) {
+			let node = arrayHolder.shift();
+			arrayHolder.push(...node.children);
+			fn(node);
+		}
+	}
+
 	depthFirstTraversal(fn) {
 		const arrayHolder = [this.root];
 
